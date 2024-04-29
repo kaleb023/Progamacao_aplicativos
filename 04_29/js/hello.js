ae550b14e4cb0b6d7dd7f8f463b38368f8957526
Daniel Kaleb <danielkallebmb@gmail.com>
2024-04-29 17:20:32 -0300

    JS

---
+++ 04_29/js/hello.js
@@ -0,0 +1,28 @@
+alert("Hello")
+
+//Meu primeiro comando 
+
+//Criando uma variavel
+let texto = "Aula Java Script"
+console.log(texto)
+let decimal = 20.8
+console.log(decimal)
+let inteiro = 20
+console.log(inteiro)
+let booleano = true
+console.log(booleano)
+//Para criar uma variavel em JavaScript não é necessario definir um tipo
+//podemos fazer a definição direta e a atribuição em seguida
+
+//Escrevendo dentro da pagina html
+document.write(texto)   
+
+
+let num1 = "10"
+let num2 = 20
+let soma = num1 + num2  //Concatenando String "10" + 20 = 1020
+console.log(soma)
+
+let dia = "Segunda"
+let saudacao = "Boa tarde"
+console.log(dia+saudacao) //Concatenando duas Strings 
